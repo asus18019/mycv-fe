3f955cd85a9646fb9be82de5174a0d7cf3b940ed
Viktor Volokhan <voloxanvitya@gmail.com>
2026-06-20 21:04:02 +0300

    fix proxy

--- proxy.ts
+++ proxy.ts
@@ -57,3 +57,7 @@ export async function proxy(req: NextRequest) {
 
   return NextResponse.next({ request: { headers: requestHeaders } });
 }
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
